4beb17624e0b85e32d663472f7c2d3e2e2dd2b89
sergey230500 <sergey230500@mail.ru>
2019-10-15 00:02:25 +0300

    Фокусировка карты

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -22,3 +22,28 @@ $(document).ready(function(){
       }
  
 });
+
+var map;
+function initMap() {
+	  var mark0 = {lat: 52.100623 , lng:23.681037  };
+	  var mark1 = {lat:  52.1069 , lng: 23.6427}
+	  map = new google.maps.Map(
+	      document.getElementById('map'), {zoom: 12, center: mark0});
+	  var marker0 = new google.maps.Marker({position: mark0, map: map});
+	  var marker1 = new google.maps.Marker({position: mark1, map: map});
+	}
+
+function fitToCoordinates(coordinates){
+	let x = coordinates.map(element => element.x);
+	let y = coordinates.map(element => element.y);
+	let maxX = Math.max(...x);
+	let maxY = Math.max(...y);
+	let minX = Math.min(...x);
+	let minY = Math.min(...y);
+	 var minPoint = new google.maps.LatLng(minY, minX);
+	 var maxPoint = new google.maps.LatLng(maxY, maxX);
+	 var bounds = new google.maps.LatLngBounds();
+	 bounds.extend(minPoint);
+	 bounds.extend(maxPoint);
+	 map.fitBounds(bounds);
+}
